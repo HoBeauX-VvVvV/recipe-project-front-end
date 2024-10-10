9bd56d65ae84a5061b4bfe84a6aab9ec6a9c3d9f
BeauX <fire-fly_7@hotmail.com>
2024-10-10 15:19:21 -0700

    auth-service

---
+++ src/services/authService.js
@@ -0,0 +1,16 @@
+import api from './api';
+
+export const signup = async (username, password) => {
+  const response = await api.post('/users/signup', { username, password });
+  return response.data;
+};
+
+export const signin = async (username, password) => {
+  const response = await api.post('/users/signin', { username, password });
+  localStorage.setItem('token', response.data.token);
+  return response.data;
+};
+
+export const signout = () => {
+  localStorage.removeItem('token');
+};
